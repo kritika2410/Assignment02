4a7c95bd57f7ade16106114a41c91775e8a4d774
kritika2410 <65334493+kritika2410@users.noreply.github.com>
2022-10-04 08:09:38 -0400

    Unit Tests
    Containing the completed unit tests.

---
+++ Assignment02/Assignment02/Assignment02.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Assignment02.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Assignment02.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b63db76f-884d-4f09-a8b2-7ec39057fa39")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Assignment02/Assignment02/Assignment02.Tests/RectangleTest.cs
@@ -0,0 +1,162 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Assignment02.Tests
+{
+    [TestClass]
+    public class RectangleTest
+    {
+        //Rec
+
+        [TestMethod]
+        public void TestGetCurrentLength1()
+        {
+            Rectangle rectangle = new Rectangle();
+
+            Assert.AreEqual(rectangle.GetCurrentLength(), 1);
+        }
+
+        [TestMethod]
+        public void TestGetCurrentLength2()
+        {
+            Rectangle rectangle = new Rectangle(4, 10);
+
+            Assert.AreEqual(rectangle.GetCurrentLength(), 4);
+        }
+
+        [TestMethod]
+        public void TestGetCurrentLength3()
+        {
+            Rectangle rectangle = new Rectangle();
+            rectangle.SetNewLength(10);
+            Assert.AreEqual(rectangle.GetCurrentLength(), 10);
+        }
+
+        [TestMethod]
+        public void TestSetNewLength1()
+        {
+            Rectangle rectangle = new Rectangle();
+            rectangle.SetNewLength(3);
+            Assert.AreEqual(rectangle.GetCurrentLength(), 3);
+        }
+
+        [TestMethod]
+        public void TestSetNewLength2()
+        {
+            Rectangle rectangle = new Rectangle();
+            rectangle.SetNewLength(18);
+            Assert.AreEqual(rectangle.GetCurrentLength(), 18);
+        }
+
+        [TestMethod]
+        public void TestSetNewLength3()
+        {
+            Rectangle rectangle = new Rectangle();
+            rectangle.SetNewLength(10000);
+            Assert.AreEqual(rectangle.GetCurrentLength(), 10000);
+        }
+
+
+
+        [TestMethod]
+        public void TestGetCurrentWidth1()
+        {
+            Rectangle rectangle = new Rectangle();
+
+            Assert.AreEqual(rectangle.GetCurrentWidth(), 1);
+        }
+
+        [TestMethod]
+        public void TestGetCurrentWidth2()
+        {
+            Rectangle rectangle = new Rectangle(4, 10);
+
+            Assert.AreEqual(rectangle.GetCurrentWidth(), 10);
+        }
+
+        [TestMethod]
+        public void TestGetCurrentWidth3()
+        {
+            Rectangle rectangle = new Rectangle();
+            rectangle.SetNewWidth(10);
+            Assert.AreEqual(rectangle.GetCurrentWidth(), 10);
+        }
+
+        [TestMethod]
+        public void TestSetNewWidth1()
+        {
+            Rectangle rectangle = new Rectangle();
+            rectangle.SetNewWidth(3);
+            Assert.AreEqual(rectangle.GetCurrentWidth(), 3);
+        }
+
+        [TestMethod]
+        public void TestSetNewWidth2()
+        {
+            Rectangle rectangle = new Rectangle();
+            rectangle.SetNewWidth(18);
+            Assert.AreEqual(rectangle.GetCurrentWidth(), 18);
+        }
+
+        [TestMethod]
+        public void TestSetNewWidth3()
+        {
+            Rectangle rectangle = new Rectangle();
+            rectangle.SetNewWidth(10000);
+            Assert.AreEqual(rectangle.GetCurrentWidth(), 10000);
+        }
+
+
+        [TestMethod]
+        public void TestGetPerimeter1()
+        {
+            Rectangle rectangle = new Rectangle();
+
+            Assert.AreEqual(rectangle.GetPerimeter(), 4);
+        }
+
+        [TestMethod]
+        public void TestGetPerimeter2()
+        {
+            Rectangle rectangle = new Rectangle(3, 5);
+
+            Assert.AreEqual(rectangle.GetPerimeter(), 16);
+        }
+
+        [TestMethod]
+        public void TestGetPerimeter3()
+        {
+            Rectangle rectangle = new Rectangle();
+            rectangle.SetNewLength(20);
+            rectangle.SetNewWidth(10);
+            Assert.AreEqual(rectangle.GetPerimeter(), 60);
+        }
+
+
+        [TestMethod]
+        public void TestGetArea1()
+        {
+            Rectangle rectangle = new Rectangle();
+
+            Assert.AreEqual(rectangle.GetArea(), 1);
+        }
+
+        [TestMethod]
+        public void TestGetArea2()
+        {
+            Rectangle rectangle = new Rectangle(3, 5);
+
+            Assert.AreEqual(rectangle.GetArea(), 15);
+        }
+
+        [TestMethod]
+        public void TestGetArea3()
+        {
+            Rectangle rectangle = new Rectangle();
+            rectangle.SetNewLength(20);
+            rectangle.SetNewWidth(10);
+            Assert.AreEqual(rectangle.GetArea(), 200);
+        }
+
+    }
+}
